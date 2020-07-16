6c262e5c6b50245f369d622f9f6292b65ca1fb61
Roger <roger.skrzypczyk@rumblefish.pl>
2020-07-16 22:29:30 +0200

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import { App } from './components/app';
+import App from './components/app';
 
-const Index = () => {
-  return <div>TEST</div> //<App brands={[]} models={[]} cities={[]} />;
-};
+// const Index = () => {
+//   return <div>TEST</div> //<App brands={[]} models={[]} cities={[]} />;
+// };
 
-ReactDOM.render(<Index />, document.getElementById("root"));
+ReactDOM.render(<App brands={[]} models={[]} cities={[]} />, document.querySelector('#root'));
