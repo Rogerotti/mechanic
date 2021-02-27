7608a2ba274842c377e6d0570d6671316e8de283
Roger <roger.skrzypczyk@rumblefish.pl>
2021-02-27 13:24:01 +0100

    use login page inside trainer project

--- packages/trainer/src/components/pages/login-page/container.tsx
+++ packages/trainer/src/components/pages/login-page/container.tsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { LoginPanel } from '@ui/composition/login-panel';
+import { LoginPage } from '@ui/pages/login-page';
 
 import Layout from '../../core/layout';
-import { Box } from '@material-ui/core';
 
 export const LoginPageContainer: React.FC = () => {
   const [username, setUsername] = useState('');
@@ -10,22 +9,18 @@ export const LoginPageContainer: React.FC = () => {
 
   return (
     <Layout>
-      <Box bgcolor="primary.main" height="500px" display="flex" justifyContent="center" alignItems="center">
-        <Box>
-          <LoginPanel
-            loginLabel="login"
-            loginPlaceholderLabel="Wprowadź nazwę użytkownika"
-            passwordPlaceholderLabel="Wprowadź hasło"
-            signinLabel="Zaloguj"
-            signupLabel="Zarejestruj"
-            separatorLabel="Lub"
-            username={username}
-            password={password}
-            onUsernameChange={(value) => setUsername(value)}
-            onPasswordChange={(value) => setPassword(value)}
-          />
-        </Box>
-      </Box>
+      <LoginPage
+        loginLabel="login"
+        loginPlaceholderLabel="Wprowadź nazwę użytkownika"
+        passwordPlaceholderLabel="Wprowadź hasło"
+        signinLabel="Zaloguj"
+        signupLabel="Zarejestruj"
+        separatorLabel="Lub"
+        username={username}
+        password={password}
+        onUsernameChange={(value) => setUsername(value)}
+        onPasswordChange={(value) => setPassword(value)}
+      />
     </Layout>
   );
 };
